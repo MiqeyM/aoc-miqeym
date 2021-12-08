8284880ccf2f55348a72b4e1bc708738ddb08144
Mikolaj <mikolajjm@gmail.com>
2021-12-08 13:34:59 +0100

    Day4 Finished

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,3 +1,5 @@
+import java.lang.Exception
+
 fun main() {
     fun createBingoArrays(inputData: List<String>): List<List<List<Int>>> {
         var tempTable = mutableListOf<List<Int>>()
@@ -52,50 +54,145 @@ fun main() {
         var solutionNumber = -1
         var i = 0
         var tempBingo = bingoTables.first()
+        var bingoIndex = bingoTables.indexOf(tempBingo)
         while (solutionNumber == -1) {
             val currentNumber = bingoNumbers[i]
-            //TODO: zmienić to na przerywalnego while
-            bingoTables.forEachIndexed { index, it ->
-                var wasNumberChecked = false
-                var rowIndex = 0
-                var itemIndex = 0
-                //Iterate until number is found in THIS table
-                while (!wasNumberChecked) {
-                    if (it[rowIndex][itemIndex] == currentNumber) {
-                        //Adding the 1 to current row result
-                        bingoSolutions[index][0][rowIndex] += 1
-                        //Adding the 1 to current column result
-                        bingoSolutions[index][1][itemIndex] += 1
-                        if (bingoSolutions[index][1][itemIndex] == 5 || bingoSolutions[index][0][rowIndex] == 5) {
-                            solutionNumber = currentNumber
-                        }
-                        wasNumberChecked = true
-                    } else {
-                        if (itemIndex == 4 && rowIndex < 4) {
-                            itemIndex = 0
-                            rowIndex++
-                        } else if (itemIndex == 4 && rowIndex == 4)
-                            throw error("Ktos tu nie znalazł numerka tak jak miał!")
-                        else
-                        itemIndex++
+            var wasNumberChecked = false
+            var isSearchFinished = false
+            var rowIndex = 0
+            var itemIndex = 0
+            //Iterate until number is found in THIS table or the end of THIS BINGO is reached
+            while (!wasNumberChecked && !isSearchFinished) {
+                if (tempBingo[rowIndex][itemIndex] == currentNumber) {
+                    //Adding the 1 to current row result
+                    bingoSolutions[bingoIndex][0][rowIndex] += 1
+                    //Adding the 1 to current column result
+                    bingoSolutions[bingoIndex][1][itemIndex] += 1
+                    if (bingoSolutions[bingoIndex][1][itemIndex] == 5 || bingoSolutions[bingoIndex][0][rowIndex] == 5) {
+                        solutionNumber = currentNumber
                     }
+                    wasNumberChecked = true
+                } else {
+                    if (itemIndex == 4 && rowIndex < 4) {
+                        itemIndex = 0
+                        rowIndex++
+                    } else if (itemIndex == 4 && rowIndex == 4)
+                        isSearchFinished = true
+                    else
+                        itemIndex++
                 }
-                if(solutionNumber!=-1)
-                    break
+            }
+            if ((isSearchFinished || wasNumberChecked) && solutionNumber == -1 && bingoIndex != bingoTables.lastIndex)
+                tempBingo = bingoTables[++bingoIndex]
+            else if (solutionNumber != -1)
+            //tempBingo = bingoTables[++bingoIndex]
+            else if (bingoIndex == bingoTables.lastIndex) {
+                i++
+                tempBingo = bingoTables.first()
+                bingoIndex = 0
             }
 
         }
-        return input.size
+        val winningBingo = tempBingo.toList()
+        val numbersCrossed = bingoNumbers.take(i + 1)
+        val a = winningBingo.flatten() - numbersCrossed.toSet()
+        val sum = a.sum()
+
+
+        return sum * solutionNumber
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val bingoNumbers = input[0].split(',').map { it.toInt() }
+        val bingoData = input.drop(2)
+        val bingoTables = createBingoArrays(bingoData).toMutableList()
+        val rows = 5
+        val columns = 5
+        val tablesNumber = bingoTables.size
+        bingoTables[1][1][1]
+        val bingoSolutions = createEmptySolutionsTable(tablesNumber, columns, rows)
+        var solutionNumber = -1
+        var i = 0
+        var tempBingo = bingoTables.first()
+        var bingoIndex = bingoTables.indexOf(tempBingo)
+        while (solutionNumber == -1) {
+            val currentNumber = bingoNumbers[i]
+            var wasNumberChecked = false
+            var isIterationFinished = false
+            var wasCurrentRemoved = false
+            var rowIndex = 0
+            var itemIndex = 0
+            //Iterate until number is found in THIS table or the end of THIS BINGO is reached
+            while (!wasNumberChecked && !isIterationFinished) {
+                //The current bingo number was found
+                if (tempBingo[rowIndex][itemIndex] == currentNumber) {
+                    //Adding the 1 to current row result
+                    bingoSolutions[bingoIndex][0][rowIndex] += 1
+                    //Adding the 1 to current column result
+                    bingoSolutions[bingoIndex][1][itemIndex] += 1
+                    //The current number caused a BINGO!
+                    if (bingoSolutions[bingoIndex][1][itemIndex] == 5 || bingoSolutions[bingoIndex][0][rowIndex] == 5) {
+                        //Check if it is the last table
+                        if (bingoTables.size > 1) {
+                            bingoTables.remove(tempBingo)
+                            bingoSolutions.removeAt(bingoIndex)
+                            wasCurrentRemoved = true
+                        } else {
+                            solutionNumber = currentNumber
+                        }
+
+                    }
+                    wasNumberChecked = true
+                } else {
+                    if (itemIndex == 4 && rowIndex < 4) {
+                        itemIndex = 0
+                        rowIndex++
+                    } else if (itemIndex == 4 && rowIndex == 4)
+                        isIterationFinished = true
+                    else
+                        itemIndex++
+                }
+            }
+            //We are still looking for the last winning BINGO!
+            if ((isIterationFinished || wasNumberChecked) && solutionNumber == -1 /*&& bingoIndex!=bingoTables.lastIndex*/) {
+                if (wasCurrentRemoved) {
+                    if (bingoIndex > bingoTables.lastIndex) {
+                        tempBingo = bingoTables.first()
+                        bingoIndex =0
+                        i++
+                    } else
+                        tempBingo = bingoTables[bingoIndex]
+
+                } else {
+                    if (bingoIndex == bingoTables.lastIndex) {
+                        i++
+                        tempBingo = bingoTables.first()
+                        bingoIndex = 0
+                    } else {
+                        try {
+                            tempBingo = bingoTables[++bingoIndex]
+                        } catch (e: Exception) {
+                        }
+                    }
+                }
+            }
+
+
+        }
+        val loosingBingo = tempBingo.toList()
+        val numbersCrossed = bingoNumbers.take(i + 1)
+        val a = loosingBingo.flatten() - numbersCrossed.toSet()
+        val sum = a.sum()
+
+
+        return sum * solutionNumber
     }
 
 
 // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 4512)
+    check(part2(testInput) == 1924)
 
     val input = readInput("Day04")
     println(part1(input))
