59617a11b54553ad11f9903ada623fb0f560332a
Mikołaj Marciniak <mikolajjm@gmail.com>
2021-12-08 22:11:30 +0100

    Day05-inprogress

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,17 +1,14 @@
-import jdk.jshell.Diag
-
 class Point(val x: Int, val y: Int) {
+    override fun toString(): String {
+
+        return "($x,$y)"
+    }
 }
 
 class Line(val start: Point, val end: Point) {
     val furthestX = maxOf(start.x, end.x)
     val furthestY = maxOf(start.y, end.y)
-
-}
-
-class Diagram(val lines: List<Line>) {
-    val crossingSet = mutableSetOf<Point>()
-
+    val isVertical = start.x == end.x
 
 }
 
@@ -32,25 +29,67 @@ fun getLineList(input: List<String>): List<Line> {
     return tempLineList
 }
 
-fun setUpDiagram(lineList: List<Line>): Diagram {
+fun setUpDiagram(lineList: List<Line>): Array<Array<Int>> {
     var maxX = 0
     var maxY = 0
     lineList.forEach { line ->
         maxX = maxOf(line.furthestX, maxX)
         maxY = maxOf(line.furthestY, maxY)
     }
-    val diagramArray = Array<Array<Int>>(maxX){ setZero(maxY)}
-    return Diagram(lineList)
+    val diagramArray = Array(maxX+1) { setZero(maxY+1) }
+    var from = 0
+    var to = 0
+    lineList.forEach { line ->
+        var i = 0
+        if (line.isVertical) {
+            from = minOf(line.start.y, line.end.y)
+            to = maxOf(line.start.y, line.end.y)
+            i = from
+            while (i <= to) {
+                diagramArray[line.start.x][i] += 1
+                i++
+            }
+
+
+        } else {
+            from = minOf(line.start.x, line.end.x)
+            to = maxOf(line.start.x, line.end.x)
+            i = from
+            while (i <= to) {
+                diagramArray[i][line.start.y] += 1
+                i++
+            }
+        }
+    }
+    return diagramArray
 }
 
-fun setZero(size : Int):  Array<Int> {
-    return Array<Int>(size){0}
+fun setZero(size: Int): Array<Int> {
+    return Array(size) { 0 }
 }
+
+fun printArray(array: Array<Array<Int>>) {
+    array.forEach { row ->
+        row.forEach { item -> print(".$item") }
+        println(";\n")
+    }
+}
+
+fun countOverlaps(diagram: Array<Array<Int>>): Int {
+    var counter = 0
+    diagram.forEach { line ->
+        line.forEach { point -> if (point > 1) counter++ }
+    }
+    return counter
+}
+
 
 fun main() {
     fun part1(input: List<String>): Int {
         val lines = removeDiagonals(getLineList(input))
-        return input.size
+        val diagram = setUpDiagram(lines)
+        //printArray(diagram)
+        return countOverlaps(diagram)
     }
 
     fun part2(input: List<String>): Int {
@@ -59,7 +98,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 5)
 
     val input = readInput("Day05")
     println(part1(input))
