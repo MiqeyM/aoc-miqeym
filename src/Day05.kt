dbf39aff4c704a9adae54e90e9d53858d140cf12
Mikolaj <mikolajjm@gmail.com>
2021-12-09 14:01:52 +0100

    Day5

--- src/Day05.kt
+++ src/Day05.kt
@@ -6,10 +6,22 @@ class Point(val x: Int, val y: Int) {
 }
 
 class Line(val start: Point, val end: Point) {
+    val isDiagonal: Boolean = (start.x != end.x && start.y != end.y)
     val furthestX = maxOf(start.x, end.x)
     val furthestY = maxOf(start.y, end.y)
     val isVertical = start.x == end.x
 
+}
+
+fun getLeftMostPoint(line: Line): Array<Point> {
+    val a = arrayOf(line.start, line.end)
+    a.sortBy { it.x }
+    return a
+
+}
+
+fun isAscending(leftmost: Point, rightMost: Point): Boolean {
+    return leftmost.y > rightMost.y
 }
 
 fun removeDiagonals(lines: List<Line>): List<Line> {
@@ -18,7 +30,7 @@ fun removeDiagonals(lines: List<Line>): List<Line> {
 
 fun getLineList(input: List<String>): List<Line> {
     val tempLineList = mutableListOf<Line>()
-    val pattern = """(\d,\d)""".toRegex()
+    val pattern = """(\d{1,3},\d{1,3})""".toRegex()
     input.forEach { line ->
         val matches = pattern.findAll(line)
         val list = matches.map { it.value }.toList()
@@ -36,31 +48,45 @@ fun setUpDiagram(lineList: List<Line>): Array<Array<Int>> {
         maxX = maxOf(line.furthestX, maxX)
         maxY = maxOf(line.furthestY, maxY)
     }
-    val diagramArray = Array(maxX+1) { setZero(maxY+1) }
+    val diagramArray = Array(maxX + 1) { setZero(maxY + 1) }
     var from = 0
     var to = 0
+    var leftMost: Point
+    var rightMost: Point
     lineList.forEach { line ->
         var i = 0
-        if (line.isVertical) {
+        if (line.isVertical && !line.isDiagonal) {
             from = minOf(line.start.y, line.end.y)
-            to = maxOf(line.start.y, line.end.y)
+            to = line.furthestY
             i = from
             while (i <= to) {
-                diagramArray[line.start.x][i] += 1
+                diagramArray[i][line.start.x] += 1
                 i++
             }
 
 
+        } else if (line.isDiagonal) {
+            val sortedPoints = getLeftMostPoint(line)
+            leftMost = sortedPoints[0]
+            rightMost = sortedPoints[1]
+            val k: Int = if (isAscending(leftMost, rightMost)) -1 else 1
+            i = 0
+            while (leftMost.x + i <= rightMost.x ) {
+                diagramArray[leftMost.y + (k*i)][leftMost.x + i] += 1
+                i++
+            }
         } else {
             from = minOf(line.start.x, line.end.x)
-            to = maxOf(line.start.x, line.end.x)
+            to = line.furthestX
             i = from
             while (i <= to) {
-                diagramArray[i][line.start.y] += 1
+                diagramArray[line.start.y][i] += 1
                 i++
             }
         }
+
     }
+
     return diagramArray
 }
 
@@ -70,9 +96,10 @@ fun setZero(size: Int): Array<Int> {
 
 fun printArray(array: Array<Array<Int>>) {
     array.forEach { row ->
-        row.forEach { item -> print(".$item") }
+        row.forEach { item -> print("   $item") }
         println(";\n")
     }
+    println("----------")
 }
 
 fun countOverlaps(diagram: Array<Array<Int>>): Int {
@@ -80,6 +107,7 @@ fun countOverlaps(diagram: Array<Array<Int>>): Int {
     diagram.forEach { line ->
         line.forEach { point -> if (point > 1) counter++ }
     }
+    //printArray(diagram)
     return counter
 }
 
@@ -93,12 +121,16 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val lines = getLineList(input)
+        val diagram = setUpDiagram(lines)
+        //printArray(diagram)
+        return countOverlaps(diagram)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day05_test")
     check(part1(testInput) == 5)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day05")
     println(part1(input))
